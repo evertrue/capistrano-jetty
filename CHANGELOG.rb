333739fafbfb77e522079b104a9cde2e499351cf
Jeff Byrnes <jeff@evertrue.com>
2014-02-11 16:14:27 -0500

    Bump to v0.0.2, add CHANGELOG

---
+++ CHANGELOG.rb
@@ -0,0 +1,11 @@
+# Capistrano::Jetty Changelog
+
+This is the Changelog for Capistrano::Jetty.
+
+## v0.0.2 (2014-02-11)
+
+* Fix incorrect `require` in gemspec
+
+## v0.0.1 (2014-02-11)
+
+* Initial release
